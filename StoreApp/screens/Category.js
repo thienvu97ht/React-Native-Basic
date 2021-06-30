08fad9a36dc240f33cd59e84c7992fa3277d6a45
ThienVu97ht <thien.vu97ht123@gmail.com>
2021-06-30 23:26:11 +0700

    10. React navigation (Stack)

--- StoreApp/screens/Category.js
+++ StoreApp/screens/Category.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function Category() {
   return (
-    <View>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Category</Text>
     </View>
   );
