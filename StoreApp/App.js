dbf4a4f62ecd2450dfc3bff091cbbc6b07fb43a7
ThienVu97ht <thien.vu97ht123@gmail.com>
2021-06-30 20:45:09 +0700

    07. ScrollView

--- StoreApp/App.js
+++ StoreApp/App.js
@@ -1,18 +1,33 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
 import CategoryListItem from "./components/CategoryListItem";
 import Images from "./contants/images";
 
 export default function App() {
-  console.log("Hello ae");
   return (
-    <View style={styles.container}>
-      <CategoryListItem title="Tao có súng đây nè!" image={Images.GUN} />
-      <CategoryListItem
-        title="Công an đến bắt mày nè!"
-        image={Images.POLYCE_CAR}
-      />
-      <CategoryListItem title="Còng tay mày lại nè!" image={Images.CONG_TAY} />
+    <View>
+      <ScrollView
+        style={{ paddingLeft: 16, paddingRight: 16 }}
+        contentContainerStyle={{ backgroundColor: "#F00" }}>
+        <CategoryListItem title="Tao có súng đây nè!" image={Images.GUN} />
+        <CategoryListItem
+          title="Công an đến bắt mày nè!"
+          image={Images.POLYCE_CAR}
+        />
+        <CategoryListItem
+          title="Còng tay mày lại nè!"
+          image={Images.CONG_TAY}
+        />
+        <CategoryListItem title="Tao có súng đây nè!" image={Images.GUN} />
+        <CategoryListItem
+          title="Công an đến bắt mày nè!"
+          image={Images.POLYCE_CAR}
+        />
+        <CategoryListItem
+          title="Còng tay mày lại nè!"
+          image={Images.CONG_TAY}
+        />
+      </ScrollView>
     </View>
   );
 }
