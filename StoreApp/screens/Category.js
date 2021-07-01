879af1182c84b27725621ea37b33ee63b1e99340
thienvu97ht <thien.vu97ht123@gmail.com>
2021-07-01 11:17:33 +0700

    12. Grid layout

--- StoreApp/screens/Category.js
+++ StoreApp/screens/Category.js
@@ -1,24 +1,55 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { FlatList, StyleSheet, View } from "react-native";
+import ProductListItem from "../components/ProductListItem";
 
-export default function Category({ route }) {
-  const { categoryName } = route.params;
+export default function Category(props) {
+  const { categoryName } = props.route.params;
+  const [products, setProducts] = useState([
+    {
+      id: 1,
+      images: [
+        {
+          url: "https://product.hstatic.net/1000281824/product/6494b116-a95f-42e1-bd99-33ae0fef78a3_c36ead4edfde4943b98b4ad16eb74463_grande.jpeg",
+        },
+      ],
+      name: "Balo Hạt Nhân Degrey",
+      price: 420000,
+    },
+    {
+      id: 2,
+      images: [
+        {
+          url: "https://product.hstatic.net/1000281824/product/1961e24d-55da-4c5a-bde3-b1d2effd58d6_4eeced7e1b174b949b332388a9e441ee_grande.jpeg",
+        },
+      ],
+      name: "Balo Degrey Quằng Quện",
+      price: 420000,
+    },
+  ]);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text__content}>{categoryName}</Text>
-    </View>
+    <FlatList
+      data={products}
+      contentContainerStyle={styles.container}
+      numColumns={2}
+      keyExtractor={(item) => `${item.id}`}
+      renderItem={({ item }) => (
+        <View style={styles.wrapper}>
+          <ProductListItem product={item} />
+        </View>
+      )}
+    />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: 16,
+    paddingHorizontal: 8,
   },
 
-  text__content: {
-    fontSize: 24,
+  wrapper: {
+    flex: 1,
+    paddingHorizontal: 8,
   },
 });
